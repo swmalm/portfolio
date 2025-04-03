8ade837b88f0b1ee200b264172504e8228a17eeb
swmalm <simon.malmstrom.sm1@gmail.com>
2025-04-03 15:15:29 +0200

    Loading screen change

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
     const [text, setText] = useState("");
-    const fullText = "<Hello World />";
+    const fullText = "<Herro Everynyan! How are you? Fine, Thank you! />";
 
     useEffect(() => {
         let index = 0;
