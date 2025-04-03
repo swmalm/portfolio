813281c29ef3aa0ef2a52b2eb4ec03782f9d58e8
swmalm <simon.malmstrom.sm1@gmail.com>
2025-04-03 18:23:26 +0200

    First version of navbar and more animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { LoadingScreen } from './components/LoadingScreen'
+import { Navbar } from './components/Navbar'
 import "./index.css"
 
 function App() {
@@ -8,6 +9,12 @@ function App() {
   return (
     <>
       {!isLoading && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div className={`min-h-screen transition-opacity duration-700 ${
+          isLoading ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <Navbar />
+      </div>
       </>
   );
 }
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
     const [text, setText] = useState("");
-    const fullText = "<Herro Everynyan! How are you? Fine, Thank you! />";
+    const fullText = "<Hello!/>";
 
     useEffect(() => {
         let index = 0;
